ba0731b10afd99fad0e8554f83d6f06cdd46087d
Haile Adugna <98773895+haileadugna@users.noreply.github.com>
2023-12-21 21:35:48 +0300

    install tailwindcss

--- askema/src/components/footer.jsx
+++ askema/src/components/footer.jsx
@@ -1,75 +1,118 @@
 import React from 'react';
-import './footer.css';
+
+
 
 export default function Footer() {
   return (
-    <div className='footer'>
-      
-
-           
-      <div className="container1">
-        <div className="reachus">
-          <h3>Reach Us</h3>
-          <div className="phonenumber">
-            <div className="phonee">
-              <p>
-              <i className="fa fa-phone"></i>
-              </p>
-              
-            </div>
-            <div>
-              <p>+251932070973</p>
-              {/* <p>+251-915-418-696</p> */}
+    <footer className="footer py-5">
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div className="mb-5 md:mb-0">
+            <h6 className="text-uppercase mb-2">Wedding of your dreams</h6>
+            <p className="mb-4 pb-2">
+              The next generation of design systems.
+            </p>
+            <div className="flex space-x-4">
+              <a href="javascript:;" className="text-secondary">
+                <span className="text-lg fab fa-facebook" aria-hidden="true"></span>
+              </a>
+              <a href="javascript:;" className="text-secondary">
+                <span className="text-lg fab fa-twitter" aria-hidden="true"></span>
+              </a>
+              <a href="javascript:;" className="text-secondary">
+                <span className="text-lg fab fa-instagram" aria-hidden="true"></span>
+              </a>
+              <a href="javascript:;" className="text-secondary">
+                <span className="text-lg fab fa-pinterest" aria-hidden="true"></span>
+              </a>
+              <a href="javascript:;" className="text-secondary">
+                <span className="text-lg fab fa-github" aria-hidden="true"></span>
+              </a>
             </div>
           </div>
-          <p>
-            <div className="fa fa-envelope"></div> bethlehemayele1@gmail.com
-          </p>
-          <p>
-            <div className="fa fa-location"></div> Phillipos ,Woreda 11,
-                Addis Ketema,
-                Addis Ababa,
-                Ethiopia
-          </p>
-        </div>
-        
-        <div className="joinourpremium">
-          <h3>Explore More</h3>
-          <ul>
-            <a href="/">
-              <li>Home</li>
-            </a>
-                  
-            <a href="/aboutus">
-              <li>AboutUs</li>
-            </a>
-            <a href="/contactus">
-              <li>ContactUs</li>
-            </a>
-          </ul>
-        </div>
-      </div>
-      <div className="row">
-      <div className="social">
-          
-          <ul>
-            <li><a href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook-f"></i></a></li>
-            <li><a href="https://twitter.com/" target="_blank"><i className="fab fa-x"></i></a></li>
-            <li><a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a></li>
-            <li><a href="https://www.linkedin.com/" target="_blank"><i className="fab fa-linkedin-in"></i></a></li>
-            <li><a href="https://www.linkedin.com/" target="_blank"><i className="fab fa-telegram"></i></a></li>
-          </ul>
+
+          <div className="col-span-2 md:col-span-1">
+            <h6 className="text-sm">Company</h6>
+            <ul className="flex flex-col space-y-2">
+              <li>
+                <a href="javascript:;" className="text-secondary">About Us</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Careers</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Press</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Blog</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-2 md:col-span-1">
+            <h6 className="text-sm">Pages</h6>
+            <ul className="flex flex-col space-y-2">
+              <li>
+                <a href="javascript:;" className="text-secondary">Login</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Register</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Add list</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-2 md:col-span-1">
+            <h6 className="text-sm">Legal</h6>
+            <ul className="flex flex-col space-y-2">
+              <li>
+                <a href="javascript:;" className="text-secondary">Terms</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">About Us</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Team</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Privacy</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="col-span-2 md:col-span-1">
+            <h6 className="text-sm">Resources</h6>
+            <ul className="flex flex-col space-y-2">
+              <li>
+                <a href="javascript:;" className="text-secondary">Blog</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Service</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Product</a>
+              </li>
+              <li>
+                <a href="javascript:;" className="text-secondary">Pricing</a>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className="col">
-          <p id='copy'>© 2023 | Developed by <strong>Hake</strong></p>
-        </div>
-        
-      </div>
+        <hr className="horizontal dark mt-8 mb-4" />
 
-      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='svgForFooter'>
-        <path fill="brown" fill-Opacity="1" d="M0,32L60,58.7C120,85,240,139,360,138.7C480,139,600,85,720,96C840,107,960,181,1080,181.3C1200,181,1320,107,1380,69.3L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-      </svg> */}
-    </div>
+        <div className="text-center">
+          <p className="text-sm text-secondary">
+            Copyright © 2022 Soft by Creative Tim & Loopple.
+          </p>
+        </div>
+      </div>
+    </footer>
+
   );
 }
